629e8f4feca6909bc0e81399bc355b76f8b70aea
IrisDuong <manhtt09@gmail.com>
2026-04-05 10:38:50 +0700

    init/initialize-backend-structure

---
+++ src/main/java/com/bing/BingadminBeApplication.java
@@ -0,0 +1,13 @@
+package com.bing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingadminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingadminBeApplication.class, args);
+	}
+
+}
