3c00f78c3b0b3c163c3e166e3c33558832f6b283
IrisDuong <manhtt09@gmail.com>
2026-04-07 15:44:15 +0700

    fix/  Add the misseing file for oauth2 exception handler
    - Add file AuthExceptionHandler

--- src/main/java/com/bing/auth/config/AuthExceptionHandler.java
+++ src/main/java/com/bing/auth/config/AuthExceptionHandler.java
@@ -2,14 +2,13 @@ package com.bing.auth.config;
 
 import java.io.IOException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import com.bing.utils.eums.AppHeader;
-import com.bing.utils.func.CommonUtils;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import com.bing.utils.eums.HttpErrorCode;
+import com.bing.utils.exception.BaseException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,12 +16,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class AuthExceptionHandler implements AuthenticationEntryPoint{
+	
+	private final HandlerExceptionResolver handlerExceptionResolver;
+	
+	private AuthExceptionHandler(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver) {
+		super();
+		this.handlerExceptionResolver = handlerExceptionResolver;
+	}
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		response.setStatus(HttpStatus.UNAUTHORIZED.value());
-		response.getWriter().write("{\"message\":\"User is unauthorized. Please login !\"}");
+		handlerExceptionResolver.resolveException(request, response, null, new BaseException(HttpErrorCode.UNAUTHORIZED));
 	}
-
 }
